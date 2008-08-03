1ff231723670255a258d9fb0fd4e7160b630e190
Petr Baudis <pasky@suse.cz>
2008-08-03 20:00:25 +0200

    tg-summary.sh: Mark branches with missing dependencies by '\!'

--- tg-summary.sh
+++ tg-summary.sh
@@ -17,11 +17,14 @@ fi
 git for-each-ref refs/top-bases |
 	while read rev name ref; do
 		name="${ref#refs/top-bases/}"
+		missing_deps=
 
 		nonempty=
 		! branch_empty "$name" || nonempty='0'
 		deps_update=' '
 		needs_update "$name" >/dev/null || deps_update='D'
+		deps_missing=' '
+		[ -z "$missing_deps" ] || deps_missing='!'
 		base_update=' '
 		branch_contains "$name" "refs/top-bases/$name" || base_update='B'
 
@@ -32,6 +35,6 @@ git for-each-ref refs/top-bases |
 			subject="(No commits)"
 		fi
 
-		printf '%s%s%s\t%-31s\t%s\n' "$nonempty" "$deps_update" "$base_update" \
+		printf '%s%s%s%s\t%-31s\t%s\n' "$nonempty" "$deps_update" "$deps_missing" "$base_update" \
 			"$name" "$subject"
 	done
