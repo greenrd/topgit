2986273479a7303a5b21c238182e1df51be2b3eb
Kirill Smelkov <kirr@landau.phys.spbu.ru>
2008-09-19 13:08:21 +0400

    tg mail: new command for mailing patches
    Petr, since you've asked for help on this, here you are:
    
        $ tg mail [NAME]
    
    a simple script to send one patch over email.
    
    All it does is
    
        - call `tg patch` for actual patch preparation
        - extract email addresses from whom and where to send a mail
        - boils down to `git send-email`
    
    It is self-hosted -- this mail was send by me with
    
        $ tg mail t/tg-mail
    
    P.S. I'm not a bash guy, please do not beat me too much...
    
    Signed-off-by: Kirill Smelkov <kirr@landau.phys.spbu.ru>

---
+++ tg-mail.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# TopGit - A different patch queue manager
+# GPLv2
+
+name=
+
+
+## Parse options
+
+while [ -n "$1" ]; do
+	arg="$1"; shift
+	case "$arg" in
+	-*)
+		echo "Usage: tg [...] mail [NAME]" >&2
+		exit 1;;
+	*)
+		[ -z "$name" ] || die "name already specified ($name)"
+		name="$arg";;
+	esac
+done
+
+# TODO refactor me into something common?
+[ -n "$name" ] || name="$(git symbolic-ref HEAD | sed 's#^refs/heads/##')"
+base_rev="$(git rev-parse --short --verify "refs/top-bases/$name" 2>/dev/null)" ||
+	die "not a TopGit-controlled branch"
+
+
+patchfile="$(mktemp -t tg-mail.XXXXXX)"
+
+$tg patch $name >"$patchfile"
+
+hlines=$(grep -n -m 1 '^---' "$patchfile" | sed 's/:---//')
+header=$(head -$(($hlines - 1)) "$patchfile")
+
+
+
+from="$(echo "$header" | grep '^From:' | sed 's/From:\s*//')"
+to="$(echo "$header" | grep '^To:' | sed 's/To:\s*//')"
+
+
+# XXX I can't get quoting right without arrays
+[ -n "$from" ] && from=(--from "$from")
+[ -n "$to"   ] && to=(--to "$to") # FIXME there could be multimple To
+
+people=()
+[ -n "$from" ] && people=("${people[@]}" "${from[@]}")
+[ -n "$to" ]   && people=("${people[@]}" "${to[@]}")
+
+
+# NOTE git-send-email handles cc itself
+git send-email "${people[@]}" "$patchfile"
+
+rm "$patchfile"
