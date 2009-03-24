greenrd/topgit

commit c32d1dd4fcafad3f9c570adeda580a90d564a3af

hooks/pre-commit.sh: don't wail on detached head for missing .top* files
This fixes a warning

	fatal: ref HEAD is not a symbolic ref

when your HEAD is detached.

And branches under refs/top-bases/ don't need these files either.

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

hooks/pre-commit.sh
@@ -17,9 +17,19 @@ tg_util
 ## Generally have fun
 
 # Don't do anything on non-topgit branch
-git rev-parse --verify "$(git symbolic-ref HEAD | sed 's/heads/top-bases/')" >/dev/null 2>&1 ||
-	exit 0
+if head_=$(git symbolic-ref -q HEAD); then
+	case "$head_" in
+		refs/heads/*)
+			git rev-parse -q --verify "${head_#refs/heads/#refs/top-bases#}" >/dev/null || exit 0;;
+		*)
+			exit 0;;
+	esac
 
+else
+	exit 0;
+fi
+
+# TODO: check the index, not the working copy
 [ -s "$root_dir/.topdeps" ] ||
 	die ".topdeps is missing"
 [ -s "$root_dir/.topmsg" ] ||

commit fcb488d51e72c7414f9beb40ad06bf529b8b38dc

hooks/pre-commit.sh: don't stop commits on non-topgit branches
This is a regression introduced by c32d1dd4fcafad3f9c570adeda580a90d56

"${head_#refs/heads/#refs/top-bases#}" doesn't substitute refs/heads by
refs/top-bases, but strips the prefix of $head_ if it starts with
"refs/heads/#refs/top-bases#".

Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

hooks/pre-commit.sh
@@ -20,7 +20,7 @@ tg_util
 if head_=$(git symbolic-ref -q HEAD); then
 	case "$head_" in
 		refs/heads/*)
-			git rev-parse -q --verify "${head_#refs/heads/#refs/top-bases#}" >/dev/null || exit 0;;
+			git rev-parse -q --verify "${head_/#refs\/heads/refs\/top-bases}" >/dev/null || exit 0;;
 		*)
 			exit 0;;
 	esac