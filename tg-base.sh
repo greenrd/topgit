3cc69314a80c54e739872085b5b8261ecc02457e
Per Cederqvist <ceder@lysator.liu.se>
2010-08-18 09:27:18 +0200

    Do exit 1, as the doc says, if not on a TopGit branch.

--- tg-base.sh
+++ tg-base.sh
@@ -5,5 +5,5 @@
 # GPLv2
 
 name="$(git symbolic-ref HEAD | sed 's#^refs/\(heads\|top-bases\)/##')"
-base_rev="$(git rev-parse --short --verify "refs/top-bases/$name" 2>/dev/null)" || exit 0
+base_rev="$(git rev-parse --short --verify "refs/top-bases/$name" 2>/dev/null)" || exit 1
 echo $base_rev
