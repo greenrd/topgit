7f8d339732ed6e77baa2d15f74a998c4ccde327e
Robin Green <greenrd@greenrd.org>
2013-04-06 21:33:00 +0100

    TopGit branch t/base-usage annihilated.

--- tg-base.sh
+++ tg-base.sh
@@ -4,26 +4,6 @@
 # (c) Per Cederqvist <ceder@lysator.liu.se>  2010
 # GPLv2
 
-## Parse options
-
-case "$1" in
-	-h|--help)
-		echo "Usage: tg [...] base [ branch... ]" >&2
-		exit 0;;
-	*)
-		break;;
-esac
-
-if [ "$#" -eq 0 ]; then
-	set -- HEAD
-fi
-
-rv=0
-for rev in "$@"; do
-	name="$( { git symbolic-ref -q "$rev" || echo "$rev"; } | sed 's#^refs/\(heads\|top-bases\)/##')"
-	git rev-parse --short --verify "refs/top-bases/$name" 2>/dev/null || {
-		rv=1
-		echo $rev is not a TopGit branch >&2
-	}
-done
-exit $rv
+name="$(git symbolic-ref HEAD | sed 's#^refs/\(heads\|top-bases\)/##')"
+base_rev="$(git rev-parse --short --verify "refs/top-bases/$name" 2>/dev/null)" || exit 1
+echo $base_rev
